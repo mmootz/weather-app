34c42c787a2cfac53e4c8f93a9f630b15e477d54
Matt Mootz <MattMootz@gmail.com>
2025-10-13 21:15:35 -0700

    added service for redis

--- app/backend/weather_api.py
+++ app/backend/weather_api.py
@@ -3,19 +3,19 @@ import requests, redis, os, json, time
 
 app = Flask(__name__)
 
-    
+try: 
+    redis_client = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379)
+except redis.exceptions.ConnectionError as e:
+        redis_client = None
+
+
 @app.route("/weather")
 def weather():
-    
-    try: 
-        r = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379)
-    except redis.exceptions.ConnectionError:
-        return jsonify({"error": "Redis couldn't connect!"}), 500
-    
+
     city = request.args.get("city", "Intercourse")
     cache_key = f"weather:{city.lower()}"
 
-    cached = r.get(cache_key)
+    cached = redis_client.get(cache_key)
     if cached:
         return jsonify(json.loads(cached))
         
@@ -27,7 +27,7 @@ def weather():
        resp.raise_for_status()
        data = resp.json()
            
-       r.setex(cache_key, 600, json.dumps(data))
+       redis_client.setex(cache_key, 600, json.dumps(data))
        return jsonify(data), 200
            
     except requests.exceptions.Timeout:
@@ -40,7 +40,20 @@ def weather():
 
 @app.route("/healthz")
 def liveness_check():
-    return "Healthy", 200
+    return jsonify({"status" : "Healthy" }) , 200
+
+@app.route("/ready")
+def readiness_check():
+    if not redis_client:
+        return jsonify({"status": "not ready", "reason": "redis client not initialized" }), 503
+    
+    try:
+        #PING REDIS
+        redis_client.ping()
+        return jsonify({"status": "ready" }), 200
+    except redis.exceptions.ConnectionError as e:
+        print(f"Readiness check failed to connect to Redis: {e}")
+        return jsonify({"status": "not ready", "reason" : "redis connection failed"}), 503
 
 
 if __name__ == '__main__':
