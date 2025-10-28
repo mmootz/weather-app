65964ee6e3ff5984c50737a065dbb78d292c23b4
mmootz <mattmootz@gmail.com>
2025-10-27 19:36:43 -0700

    fixed linter issue

--- app/backend/weather_api.py
+++ app/backend/weather_api.py
@@ -12,9 +12,9 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 try:
-    REDIS_CLIENT = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379)
+    redis_client = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379)
 except redis.exceptions.ConnectionError as error:
-    REDIS_CLIENT = None
+    redis_client = None
 
 @app.route("/weather")
 def weather():
@@ -26,7 +26,7 @@ def weather():
     city = request.args.get("city", "Intercourse")
     cache_key = f"weather:{city.lower()}"
 
-    cached = REDIS_CLIENT.get(cache_key)
+    cached = redis_client.get(cache_key)
     if cached:
         return jsonify(json.loads(cached))
 
@@ -38,7 +38,7 @@ def weather():
         resp.raise_for_status()
         data = resp.json()
 
-        REDIS_CLIENT.setex(cache_key, 600, json.dumps(data))
+        redis_client.setex(cache_key, 600, json.dumps(data))
         return jsonify(data), 200
 
     except requests.exceptions.Timeout:
@@ -60,12 +60,12 @@ def readiness_check():
     """
     readniess probe to see if redis is connected.
     """
-    if not REDIS_CLIENT:
+    if not redis_client:
         return jsonify({"status": "not ready", "reason": "redis client not initialized" }), 503
 
     try:
         #PING REDIS
-        REDIS_CLIENT.ping()
+        redis_client.ping()
         return jsonify({"status": "ready" }), 200
     except redis.exceptions.ConnectionError as redis_ping_error:
         print("Readiness check failed to connect to Redis:", redis_ping_error)
