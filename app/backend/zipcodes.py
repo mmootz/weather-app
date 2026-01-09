9e04f2ff8fb5ba0f1defeb20c0f751eb08e835be
Matt Mootz <MattMootz@gmail.com>
2026-01-08 16:46:16 -0800

    added zipcodes for static lookup

---
+++ app/backend/zipcodes.py
@@ -0,0 +1,28 @@
+import os
+import csv
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+ZIPCODES = {}
+
+zipcodedb = os.path.join(current_dir, "data/uszips.csv")
+def load_zipcodes(path=zipcodedb):
+    global ZIPCODES
+    with open(path) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            ZIPCODES[row["zip"]] = (
+                float(row["lat"]),
+                float(row["lng"]),
+            )
+
+
+def zipcode_lookup(zipcode):
+    """
+    Docstring for zipcode_lookup
+    takes a zipcode and returns lat and log
+    """
+    try:
+        return ZIPCODES[str(zipcode)]
+    except KeyError:
+        raise ValueError(f"Invalid ZIP code: {zipcode}")
